b0147db544c5c66c53275e7aebedb6115854e3cb
Lavro Olena <55317117+ElenaLavro@users.noreply.github.com>
2021-09-03 23:53:48 +0300

    Initial commit

---
+++ src/main/java/com/example/my_online_store/model/entity/Cart.java
@@ -0,0 +1,64 @@
+package com.example.my_online_store.model.entity;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "cart")
+public class Cart implements Serializable {
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long cartId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private User user;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true, mappedBy = "cart")
+    private Set<ProductInOrder> products = new HashSet<>();
+
+    public Cart() {
+    }
+
+    public Cart(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "cartId=" + cartId +
+                ", user=" + user +
+                ", products=" + products +
+                '}';
+    }
+
+    public Long getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(Long cartId) {
+        this.cartId = cartId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Set<ProductInOrder> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<ProductInOrder> products) {
+        this.products = products;
+    }
+}
---
+++ src/main/java/com/example/my_online_store/model/entity/UserStatus.java
@@ -0,0 +1,5 @@
+package com.example.my_online_store.model.entity;
+
+public enum UserStatus {
+    ACTIVE, SUSPENDED
+}
