58e6c044d9d552d7966cb14ad3122747528ae610
Lavro Olena <55317117+ElenaLavro@users.noreply.github.com>
2021-09-04 23:15:39 +0300

    Updated repository layer.

--- src/main/java/com/company/my_online_store/repository/ProductRepository.java
+++ src/main/java/com/company/my_online_store/repository/ProductRepository.java
@@ -1,14 +1,15 @@
 package com.company.my_online_store.repository;
 
 import com.company.my_online_store.model.entity.ProductEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, String> {
+    @Query("select p from ProductEntity p where p.id = :id")
     ProductEntity findProductById(UUID id);
 
-    Page<ProductEntity> findAllByOrderByProductId(Pageable pageable);
+
+//    Page<ProductEntity> findAllByOrderByProductId(Pageable pageable);
 }
--- src/main/java/com/company/my_online_store/repository/UserRepository.java
+++ src/main/java/com/company/my_online_store/repository/UserRepository.java
@@ -2,12 +2,26 @@ package com.company.my_online_store.repository;
 
 
 import com.company.my_online_store.model.entity.User;
+import com.company.my_online_store.model.entity.enums.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.Collection;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User,String> {
-    User findByEmail(String email);
-    Collection<User> findAllByRole(String role);
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByEmailOrNickname(String email, String nickname);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+
+    void deleteByEmail(String email);
+
+    @Query("update User u set u.status = :status where u.email = :email")
+    @Modifying
+    void chageStatusByEmail(String email, UserStatus status);
+
 }
