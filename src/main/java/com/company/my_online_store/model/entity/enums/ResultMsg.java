b54db5c8cd19b2f5532c9a2d58c1ab5854ab154b
Lavro Olena <55317117+ElenaLavro@users.noreply.github.com>
2021-09-10 10:59:32 +0300

    Updated code

--- src/main/java/com/company/my_online_store/model/entity/enums/ResultMsg.java
+++ src/main/java/com/company/my_online_store/model/entity/enums/ResultMsg.java
@@ -1,17 +1,17 @@
 package com.company.my_online_store.model.entity.enums;
 
 public enum ResultMsg {
-    PARAN_ERROR(1, "Parameter Error!"),
-    PRODUCT_NOT_EXIST(10, "product does not exist!"),
-    PRODUCT_NOT_ENOUGHT(11, "Not enought products in stock!"),
-    PRODUCT_STATUS_ERROR(12, "Status is incorrect!"),
-    PRODUCT_NOT_IN_CART(14, "Product is not in the cart!"),
-    CART_CHECKOUT_SUCCESS(20, "Checkout successfully!"),
-    CATEGORY_NOT_FOUND(30, "Checkout does not exist!"),
-    ORDER_NOT_FOUND(60, "Order is not exist!"),
-    ORDER_STATUS_ERROR(61, "Status is not correct"),
+    PARAM_ERROR(400, "Parameter Error!"),
+    PRODUCT_NOT_EXIST(404, "product does not exist!"),
+    PRODUCT_NOT_ENOUGHT(406, "Not enough products in stock!"),
+    PRODUCT_STATUS_ERROR(404, "Product status is incorrect!"),
+    PRODUCT_NOT_IN_CART(404, "Product is not in the cart!"),
+    CART_CHECKOUT_SUCCESS(200, "Checkout successfully!"),
+    CATEGORY_NOT_FOUND(404, "Checkout does not exist!"),
+    ORDER_NOT_FOUND(404, "Order is not exist!"),
+    ORDER_STATUS_ERROR(404, "Order status is not correct"),
     VALID_ERROR(50, "Wrong information"),
-    USER_NOT_FOUND(50, "User is not found!");
+    USER_NOT_FOUND(404, "User is not found!");
 
     private Integer code;
     private String message;
